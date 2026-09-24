bc3ad811c06eb34b26ccfd16383e43cc20f87f75
Joao Pedro <ppvidalsantos@gmail.com>
2026-09-23 22:10:27 -0300

    feat: implementa testes de unidade para sucesso

--- tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,9 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
 
 namespace Validators.Tests.Expenses.Register;
 
-internal class RegisterExpenseValidatorTests
+public class RegisterExpenseValidatorTests
 {
+    [Fact]
+    public void Success()
+    {
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+    
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeTrue();
+    }
 }
